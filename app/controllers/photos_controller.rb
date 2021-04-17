0cd8dddee1b0ab6474125f226a72128478125564
camilo gonzalez <camilogzlez@gmail.com>
2021-04-17 14:14:46 -0300

    basic estructure done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,7 +1,29 @@
 class PhotosController < ApplicationController
-
-  def index
+  skip_before_action :authenticate_user!
+  
+ def index
     @photos = Photo.all
   end
   
+  def new
+    @photo = Photo.new
+  end
+
+  def create
+    @photo = Photo.new(photo_params)
+    @photo.user = current_user
+    if @photo.save
+      redirect_to photos_path(@photo)
+    else
+      raise
+      render :new
+    end
+  end
+  
+
+  private
+
+  def photo_params
+    params.require(:photo).permit(:title, :description, :date, :location, :photo)
+  end
 end
