bd71c1e3554d31d472c12f4bd88d77c4b4a53eaf
camilo gonzalez <camilogzlez@gmail.com>
2024-10-06 15:27:52 +0200

    puma file updated

--- config/puma.rb
+++ config/puma.rb
@@ -5,7 +5,8 @@ threads min_threads_count, max_threads_count
 worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Use the port provided by Render
-port ENV.fetch("PORT") { 3000 }
+port = ENV.fetch("PORT") { 3000 }
+bind "tcp://0.0.0.0:#{port}"  # This line was incorrect in your configuration
 
 # Set the environment
 environment ENV.fetch("RAILS_ENV") { "development" }
